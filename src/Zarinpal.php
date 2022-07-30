c84edb4e49bee34c4222280337643ee55a2ea11f
Ismail Zare <zarehesmaiel@gmail.com>
2022-07-30 10:21:11 +0430

    fix bugs

--- src/Zarinpal.php
+++ src/Zarinpal.php
@@ -4,23 +4,24 @@ namespace Armincms\Zarinpal;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\{Text, Select};
-use Armincms\Arminpay\Contracts\{Gateway, Billing}; 
+use Armincms\Arminpay\Contracts\{Gateway, Billing};
 use Shetabit\Payment\Facade\Payment;
 use Shetabit\Multipay\Invoice;
 
 class Zarinpal implements Gateway
-{ 
+{
+
     /**
      * The gateway configuration values.
-     * 
+     *
      * @var array
      */
-    public $config = [];    
+    public $config = [];
 
     /**
      * Construcy the instance.
-     * 
-     * @param array $config 
+     *
+     * @param array $config
      */
     public function __construct($config)
     {
@@ -29,62 +30,66 @@ class Zarinpal implements Gateway
 
     /**
      * Make payment for the given Billing.
-     * 
-     * @param  \Illuminate\Http\Request  $request  
-     * @param  \Armincms\Arminpay\Contracts\Billing $billing  
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Armincms\Arminpay\Contracts\Billing $billing
      * @return \Symfony\Component\HttpFoundation\Response
-     * 
+     *
      * @throws \InvalidArgumentException
      */
     public function pay(Request $request, Billing $billing)
-    {    
-        return Payment::via('zarinpal')
+    {
+        return (string) Payment::via('zarinpal')
                         ->config(array_merge($this->getConfigurations(), [
-                        	'callbackUrl' => $billing->callback(),
+                            'callbackUrl' => $billing->callback(),
                         ]))
                         ->purchase($this->newInvoice($billing))
                         ->callbackUrl($billing->callback())
                         ->pay();
-    } 
+    }
 
     public function newInvoice(Billing $billing)
     {
         return tap(new Invoice, function($invoice) use ($billing) {
-            $invoice->amount(currency($billing->amount(), $billing->currency(), 'IRT', false))
+            $invoice->amount(intval($billing->amount()))
                     ->uuid($billing->getIdentifier());
         });
     }
 
+
     /**
      * Verify the payment for the given Billing.
-     * 
-     * @param  \Illuminate\Http\Request  $request  
-     * @param  \Armincms\Arminpay\Contracts\Billing $billing  
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Armincms\Arminpay\Contracts\Billing $billing
      * @return \Symfony\Component\HttpFoundation\Response
-     * 
+     *
      * @throws \InvalidArgumentException
      */
     public function verify(Request $request, Billing $billing)
     {
-        return Payment::amount(currency($billing->amount(), $billing->currency(), 'IRT', false))
+        return Payment::amount(intval($billing->amount()))
                     ->via('zarinpal')
                     ->config($this->getConfigurations())
+                    ->transactionId($billing->getIdentifier())
                     ->verify()
                     ->getReferenceId();
-    } 
- 
+    }
+
+
+
     /**
      * Returns configuration fields.
-     * 
-     * @return array 
+     *
+     * @return array
      */
     public function fields(Request $request): array
     {
-        return [ 
+        return [
             Text::make('Merchant ID', 'merchantId')
                 ->help(__('Please enter the given the Zarinpal bank Merchant Id.'))
                 ->required()
-                ->rules('required'), 
+                ->rules('required'),
 
             Select::make(__('Mode'), 'mode')->options([
                 'normal' => __('Normal (default)'),
@@ -94,10 +99,10 @@ class Zarinpal implements Gateway
             ->required()
             ->rules('required'),
         ];
-    }  
+    }
 
     public function getConfigurations()
-    {   
-        return $this->config; 
+    {
+        return $this->config;
     }
 }
